e2369cd08d97212b7eaf8027a00dca68c6be2ee6
Relin <relindmitrii@gmail.com>
2022-05-03 15:13:12 +0300

    add data

--- app/src/main/java/ru/udemy/composition/presentation/MainActivity.kt
+++ app/src/main/java/ru/udemy/composition/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.udemy.composition
+package ru.udemy.composition.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.udemy.composition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
